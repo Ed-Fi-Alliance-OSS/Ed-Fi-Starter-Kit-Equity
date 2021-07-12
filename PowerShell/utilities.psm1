dd186c4b5db38210d5b63224d693761f233180b8
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2021-07-12 14:33:37 -0600

    Removes packer and powershell scripts in order to keep them in Packer-For-Starter-Kits repository.

--- PowerShell/utilities.psm1
+++
@@ -1,9 +0,0 @@
-function Test-ApiUrl {
-    param (
-        [String] $apiUrl
-    )
-    if ([String]::IsNullOrEmpty($apiUrl)) {
-        Write-Error "No API Url configured. Edit configuration.json and run install again."
-        Exit -1
-    }
-}
